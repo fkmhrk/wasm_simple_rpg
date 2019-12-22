545e6e1c81e0c1a70ca9a42cb29fccab078af578
fkm <fkm@fkmsoft.jp>
2019-12-23 00:20:42 +0900

    Revert "Add version and template cache"

--- src/Application.ts
+++ src/Application.ts
@@ -4,14 +4,12 @@
 
 import { getBody, isStatus200 } from "./clients/Functions";
 import TopPage from "./pages/TopPage";
-import { APP_VERSION } from "./version";
 
 declare var Go: any;
 declare function init(): any;
 
 export default class Application implements IApplication {
   private templateClient: HTTPClient;
-  private templateCache: { [key: string]: string } = {};
   services: IServices;
 
   constructor(templateClient: HTTPClient, services: IServices) {
@@ -33,18 +31,10 @@ export default class Application implements IApplication {
   }
 
   fetchTemplate(name: string): Promise<string> {
-    const template = this.templateCache[name];
-    if (template != null) {
-      return Promise.resolve(template);
-    }
-    const url = `./pages/${name}?v=${APP_VERSION}`;
+    const url = `./pages/${name}`;
     return this.templateClient
       .send(Method.GET, url, {}, null)
       .then(isStatus200)
-      .then(getBody)
-      .then((body: string) => {
-        this.templateCache[name] = body;
-        return body;
-      });
+      .then(getBody);
   }
 }
